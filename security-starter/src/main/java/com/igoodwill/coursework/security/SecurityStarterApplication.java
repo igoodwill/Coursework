538bbcc70b1a26ddcf1f90c2e40eb226b2056fba
igoodwill <igor.goodwill1@gmail.com>
2019-12-27 12:23:38 +0200

    Security starter: init commit

---
+++ security-starter/src/main/java/com/igoodwill/coursework/security/SecurityStarterApplication.java
@@ -0,0 +1,13 @@
+package com.igoodwill.coursework.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityStarterApplication.class, args);
+	}
+
+}
---
+++ security-starter/src/main/java/com/igoodwill/coursework/security/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.igoodwill.coursework.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .oidcUserService(oidcUserService);
+    }
+}
